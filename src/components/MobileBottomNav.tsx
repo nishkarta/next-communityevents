c7ab54c0f3ba320418d7441dcd7dda6c1592bfa0
Tobias Halomoan <agyasta1808@gmail.com>
2024-08-30 18:27:19 +0700

    Feat : Included icons for mobile bottom nav

--- src/components/MobileBottomNav.tsx
+++ src/components/MobileBottomNav.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { usePathname } from "next/navigation";
+import { House, Users, Church, Settings } from "lucide-react";
 
 const MobileBottomNav = () => {
 	const pathname = usePathname();
@@ -14,9 +15,22 @@ const MobileBottomNav = () => {
 	return (
 		<nav className='fixed bottom-0 w-full bg-white shadow-lg md:hidden'>
 			<ul className='flex justify-around p-4'>
-				<li className='text-center cursor-pointer'>Home</li>
-				<li className='text-center cursor-pointer'>Search</li>
-				<li className='text-center cursor-pointer'>Profile</li>
+				<li className='text-center cursor-pointer'>
+					<House className='mx-auto' />
+					Home
+				</li>
+				<li className='text-center cursor-pointer'>
+					<Users className='mx-auto' />
+					Community
+				</li>
+				<li className='text-center cursor-pointer'>
+					<Church className='mx-auto' />
+					Ministry
+				</li>
+				<li className='text-center cursor-pointer'>
+					<Settings className='mx-auto' />
+					Settings
+				</li>
 			</ul>
 		</nav>
 	);
